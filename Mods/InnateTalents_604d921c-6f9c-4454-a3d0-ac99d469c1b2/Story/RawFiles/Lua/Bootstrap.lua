3b63db60853bd2bae503448546fdbd4e57d26d6f
LaughingLeader <laughingleader@gmail.com>
2020-02-13 22:53:22 -0600

    Ignore player stats (CC talent preset bug)
    If player stats have talents, they seem to get refunded when changing presets in CC

--- Mods/InnateTalents_604d921c-6f9c-4454-a3d0-ac99d469c1b2/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/InnateTalents_604d921c-6f9c-4454-a3d0-ac99d469c1b2/Story/RawFiles/Lua/Bootstrap.lua
@@ -1,4 +1,5 @@
 local player_stats = {
+	--["_Base"] = true,
 	["_Hero"] = true,
 	["HumanFemaleHero"] = true,
 	["HumanMaleHero"] = true,
@@ -31,38 +32,40 @@ local player_stats = {
 }
 
 local function ModuleLoad()
-	Ext.Print("===================================================================")
 	Ext.Print("[InnateTalents:Bootstrap.lua] Adding The Pawn/Savage Sortilege to all Character stats.")
+	Ext.Print("===================================================================")
 	local totalOverrides = 0
 	for i,stat in pairs(Ext.GetStatEntries("Character")) do
-		local talents = Ext.StatGetAttribute(stat, "Talents")
-		local next_talents = talents
+		if not player_stats[stat] then
+			local talents = Ext.StatGetAttribute(stat, "Talents")
+			local next_talents = talents
 
-		if talents == nil or talents == "" then
-			next_talents = "QuickStep;ViolentMagic"
-		else
-			if talents:find("QuickStep", 1, true) ~= nil then
-				Ext.Print("[InnateTalents:Bootstrap.lua] Stat ("..stat..") already has The Pawn ("..talents.."). Skipping.")
+			if talents == nil or talents == "" then
+				next_talents = "QuickStep;ViolentMagic"
 			else
-				next_talents = next_talents..";QuickStep"
+				if talents:find("QuickStep", 1, true) ~= nil then
+					Ext.Print("[InnateTalents:Bootstrap.lua] Stat ("..stat..") already has The Pawn ("..talents.."). Skipping.")
+				else
+					next_talents = next_talents..";QuickStep"
+				end
+				if talents:find("ViolentMagic", 1, true) ~= nil then
+					Ext.Print("[InnateTalents:Bootstrap.lua] Stat ("..stat..") already has Savage Sortilege ("..talents.."). Skipping.")
+				else
+					next_talents = next_talents..";ViolentMagic"
+				end
 			end
-			if talents:find("ViolentMagic", 1, true) ~= nil then
-				Ext.Print("[InnateTalents:Bootstrap.lua] Stat ("..stat..") already has Savage Sortilege ("..talents.."). Skipping.")
-			else
-				next_talents = next_talents..";ViolentMagic"
-			end
+
+			-- if player_stats[stat] == true then
+			-- 	if next_talents == "" then
+			-- 		next_talents = next_talents .. "AnimalEmpathy"
+			-- 	else
+			-- 		next_talents = next_talents .. ";AnimalEmpathy"
+			-- 	end
+			-- end
+
+			Ext.StatSetAttribute(stat, "Talents", next_talents)
+			totalOverrides = totalOverrides + 1
 		end
-
-		if player_stats[stat] == true then
-			if next_talents == "" then
-				next_talents = next_talents .. "AnimalEmpathy"
-			else
-				next_talents = next_talents .. ";AnimalEmpathy"
-			end
-		end
-
-		Ext.StatSetAttribute(stat, "Talents", next_talents)
-		totalOverrides = totalOverrides + 1
 	end
 	Ext.Print("[InnateTalents:Bootstrap.lua] Added the The Pawn/Savage Sortilege talent to "..tostring(totalOverrides).." Character stats.")
 	Ext.Print("===================================================================")
@@ -70,7 +73,7 @@ end
 
 Ext.RegisterListener("ModuleLoading", ModuleLoad)
 
-function LLINNATE_AddTalent(character, talent)
+function LLINNATE_Ext_AddTalent(character, talent)
 	if CharacterHasTalent(character, talent) ~= 1 then
 		if Ext.Version() >= 40 then
 			Osi.NRD_CharacterSetPermanentBoostTalent(character, talent, 1)
@@ -79,4 +82,8 @@ function LLINNATE_AddTalent(character, talent)
 			Ext.Print("[InnateTalents:Bootstrap.lua] *WARNING* The extender is version ("..Ext.Version()..") but it requires >= 40 to add talents to NPCs!")
 		end
 	end
+end
+
+function LLINNATE_Ext_TraceStats(character)
+	Ext.Print("Character("..character..") stats: ("..tostring(Ext.GetCharacter(character).Stats.Name)..") root template stats ("..GetStatString(character)..")")
 end
